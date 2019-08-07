73087f710dfccb532898e204b1f39245587b7ae7
Jonathan Redwine <jredwine1@gmail.com>
2019-08-07 09:49:59 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QueueSimulation
+//
+//  Created by Jonathan Redwine on 8/7/19.
+//  Copyright © 2019 JonathanRedwine. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
